1a56b4fb15de242a369ac135bedc673eb74ad611
alex nham <alexnham11@gmail.com>
2024-02-05 03:58:10 -0500

    dwada

--- src/GPT/config.js
+++ src/GPT/config.js
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 export const APIKEY = "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";
+=======
+export const APIKEY = "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";
+>>>>>>> parent of db36ee12 (key change)
 =======
 export const APIKEY = "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";
 >>>>>>> parent of db36ee12 (key change)
